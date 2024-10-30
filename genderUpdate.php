85738e7bdf89a5c944da1d5590d4797d31c75df6
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2024-10-30 17:37:40 +0800

    third commit

--- genderUpdate.php
+++ genderUpdate.php
@@ -4,43 +4,43 @@
     include 'includes/head.php';
 ?>
 <body>
-    <form action="genderUpdate.php" method="post">
+    <?php
+        // Selecting the exact ID of gender that i want to update it and display the same value
+        $updateID = $_GET['updateID'];
 
-        <?php
-            // Selecting the exact ID of gender that i want to update it and display the same value
-            $updateID = $_GET['updateID'];
+        $selectGender = "SELECT * FROM gender WHERE genderID='$updateID'";
+        
+        include_once "functions/queryExecute.php";
+        $gender = queryExecute($selectGender);
+        
+        foreach ($gender as $genderRows)
+        {
+    ?>
+    <form action="" method="post">
 
-            $selectGender = "SELECT * FROM gender WHERE genderID='$updateID'";
-        
-            include "functions/queryExecute.php";
-            $gender = queryExecute($selectGender);
-        
-            foreach ($gender as $genderRows)
-            {
-        ?>
-                <input type="hidden" name="inputID" value="<?php echo $genderRows['genderID'] ?>">
-                <input type="text" name="inputName" value="<?php echo $genderRows['genderName'] ?>" required>
-        <?php
-            }
-        ?>
+        <input type="hidden" name="inputID" value="<?php echo $genderRows['genderID']; ?>">
+        <input type="text" name="inputName" value="<?php echo $genderRows['genderName']; ?>" required>
 
         <button type="submit" name="update-gender-button">Enter</button>
-        <button onclick="cancelUpdate">Cancel</button>
-        
-    </form>
 
+    </form>
     <?php
+        }
+
         // Updating all the value from gender table with exact ID 
         if (isset($_POST['update-gender-button']))
         {
-            $inputID = $_POST['inpitID'];
+            $inputID = $_POST['inputID'];
             $inputName = $_POST['inputName'];
 
             $updateGender = "UPDATE gender SET genderName='$inputName' WHERE genderID='$inputID'";
 
-            include "functions/queryExecute.php";
+            include_once "functions/queryExecute.php";
             queryExecute($updateGender);
+
+            header('location: gender.php');
         }
+        $mysqli->close();
     ?>
 </body>
 </html>
